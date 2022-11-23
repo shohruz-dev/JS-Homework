8353d31445406e196e67bdb4cb530c228cacf561
Shohruz <shokhruzisroilov@gmail.com>
2022-11-23 12:48:57 +0500

    js homework

---
+++ 2-dars/js/script.js
@@ -0,0 +1,17 @@
+const obj = {
+	firstname: "Shohruz",
+	lastname: "Isroilov",
+	age:"19",
+	education: "Tatu Qarshi filalida",
+	course: "2-kursda O'qiyman",
+}
+	console.log(obj);
+
+	let arr = [{job:"developer"},{experience:"1 years"} ["white","green","red","blue"] ]
+	console.log(arr[0].job)
+
+	let prom = prompt("Birinchi sonni kiriting");
+	let prom2= prompt("Keyingi son kiriting");
+	console.log(Number(prom) % Number(prom2));
+	console.log( "first number "+ prom+" "+ "last number "+ prom2);
+
