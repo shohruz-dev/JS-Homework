87082168a4ffb0dfe198378463e33f94d423736b
Shohruz <shokhruzisroilov@gmail.com>
2022-11-23 14:36:37 +0500

    js homework

--- 2-dars/js/script.js
+++ 2-dars/js/script.js
@@ -10,7 +10,19 @@
 // 	let arr = [{job:"developer"},{experience:"1 years"} ["white","green","red","blue"] ]
 // 	console.log(arr[0].job)
 
+// 	let prom = prompt("Birinchi sonni kiriting");
+// 	let prom2= prompt("Keyingi son kiriting");
+// 	console.log(Number(prom) % Number(prom2));
+// 	console.log( "first number "+ prom+" "+ "last number "+ prom2);
+
+
+
+	
+	let prom = prompt("Birinchi sonni kiriting");
+	let prom2= prompt("Keyingi son kiriting");
 	let num1 = prompt("Birinchi sonni kiriting");
 	let num2= prompt("Keyingi son kiriting");
 	console.log (num1 % num2)
+
+
 
