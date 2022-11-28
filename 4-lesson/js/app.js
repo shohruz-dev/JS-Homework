76e40c08d0e80eb7e52a874b98d4ac729ae3df51
Shohruz <shokhruzisroilov@gmail.com>
2022-11-28 10:16:31 +0500

    while vs do while

---
+++ 4-lesson/js/app.js
@@ -0,0 +1,59 @@
+// let num = 0;
+// while (num < 10){
+//     console.log('hello');
+//     console.log(num++);
+// }
+// let num2 = 0;
+// do{
+//     console.log("hello");
+// }
+
+// let age = prompt("your");
+
+// switch (age){
+//     case '20':
+//     alert('your');
+//     break;
+//     default:
+//     alert('Siz unaqa yoshda emasiz');
+// }
+
+// let num = +prompt('your code');
+// switch (num > 3){
+//     case 1234:
+//     alert('open');
+//     break;
+//     default:
+//     alert('eror');
+// }
+
+// let num2 = 0;
+// while  (num2 < 3){
+//     num2++;
+// }
+
+
+// week day
+
+// let num = +prompt('week')
+// switch (num ){
+//     case 1:
+//     alert('Dushanba');
+//     case 2:
+//     alert('Seshanba');
+//     case 3:
+//     alert('Chorshanba');
+//     case 4:
+//     alert('Payshanba');
+//     case 5:
+//     alert('Juma');
+//     case 6:
+//     alert('Payshanba');
+//     case 7:
+//     alert('Shanba');
+//     break;
+//     default:
+//         alert('boshqa')
+// }
+
+console.log('Assalomu alaykum');
