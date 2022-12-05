5b4bd17811311772674daddfa1558b5ae528a4af
Shohruz <shokhruzisroilov@gmail.com>
2022-12-05 12:27:14 +0500

    js homework

--- L-operators/js/app.js
+++ L-operators/js/app.js
@@ -1,6 +1,13 @@
-let pr = prompt('Ismingizni kiriting');
-    // console.log('shohruz' && 'Shohruz');
-if(30>age || 50<age){
-    console.log(pr);
+let numberOfFilms = +prompt('Qancha film ko\'rgansiz?');
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 }
-    
+let lastFilms = prompt('Oxirgi ko\'rgan filimlaringizdan biri?');
+let priceFilms = prompt('Uni qancha baholagan bo\'lardingiz?');
+if(){
+
+}
