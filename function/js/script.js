12e42043698df6e29cd3c9ca910b6537a7914874
Shohruz <shokhruzisroilov@gmail.com>
2022-12-07 15:12:16 +0500

    homework

--- function/js/script.js
+++ function/js/script.js
@@ -1,14 +1,14 @@
-for (let i = 0; i < 3; i++){
-let firstName = prompt('Ismingizni kiriting');
-let lastName = prompt('Familiyangizni kiriting');
-let age = prompt('Yoshingizni kiriting');
-function setInfo(){
-	students = {
-	firstName:firstName,
-	lastName:lastName,
-	age:age,
+let Infos = {}
+for (let i = 0; i < 4; i++) {
+	let firstName = prompt('Ismingizni kiriting')
+	let lastName = prompt('Familiyangizni kiriting')
+	let age = prompt('Yoshingizni kiriting')
+
+	function setInfo(firsName, lastname, age) {
+		return {
+			firsName,lastName,age
+		}
 	}
+	Infos['student' + i] = setInfo(firstName, lastName, age);
 }
-setInfo();
-	console.log(students);
-}
+	console.log(Infos);
