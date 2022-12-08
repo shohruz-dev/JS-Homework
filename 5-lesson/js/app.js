bbbde19f96f705e46f3e4a5b622cd2e4def308d9
Shohruz <shokhruzisroilov@gmail.com>
2022-12-08 11:34:42 +0500

    js homework

--- 5-lesson/js/app.js
+++
@@ -1,8 +0,0 @@
-let result = fizzBuzz(Son)
-    console.log(result)
-function fizzBuzz (input){
-    if (typeof input !== 'number')
-        return 'Son emas'
-    if (input %3 ===0) $$ (input %5 ===0)
-        return 'fizzBazz'
-}
