a45ba81b9993180833ba7cacadad679c28e09264
Shohruz <shokhruzisroilov@gmail.com>
2022-12-09 14:42:57 +0500

    js homework

--- 7.Numbers/js/script.js
+++ 7.Numbers/js/script.js
@@ -1,17 +1,4 @@
-// 1 - function pow(x, n)
-//pow(2, 3) 8
-
-//2 - function floor(n)
-//floor(2.7324234) 2
-
-//3 - function ceil(n)
-//ceil(2.7)  3
-
-//4 - function round(n)
-//round(2.7)  3
-//round(2.7)  2
-
-
+//homework
 
 // 1 - function pow(x, n)
 //pow(2, 3) 8
@@ -22,8 +9,26 @@ console.log(degree(2,3));
 
 //2 - function floor(n)
 //floor(2.7324234) 2
+let number = 2.7324;
+console.log(~number  * -1 - 1); 
 
+//3 - function ceil(n)
+//ceil(2.7)  3
+function RoundNum(number){
+  var c = number % 1;
+  return number-c+(c/1+1.5>>1)*1
+}
+console.log(RoundNum(2.7324234));
 
+//4 - function round(n)
+//round(2.7)  3
+//round(2.7)  2
+function RoundNum(number){
+  var c = number % 1;
+  return number-c+(c/1+1.5>>1)*1
+}
+
+console.log(RoundNum(2.7));
 
 //5 - function sqrt(x, n)
 //sqrt(16, 2)  4
