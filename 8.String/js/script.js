007030dbefb46dd71afc8502bb615a9a2c251b98
Shohruz <shokhruzisroilov@gmail.com>
2022-12-16 15:00:49 +0500

    Js homework

---
+++ 8.String/js/script.js
@@ -0,0 +1,9 @@
+//indexof, includes, startsWith, endsWith, slice, substring, substr
+
+function indexOf(str, searchString, pos = 0)
+
+
+let example = "Open Web"
+indexOf(example, 'e') // 2
+
+// 'str'.indexOf('search')
