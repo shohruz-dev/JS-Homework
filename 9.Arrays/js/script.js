915afd0141e2436adb23bfd7e2424d80c425f426
Shohruz <shokhruzisroilov@gmail.com>
2022-12-19 15:55:03 +0500

    js homework

--- 9.Arrays/js/script.js
+++ 9.Arrays/js/script.js
@@ -1,16 +1,15 @@
 //Homework
 //pop
-let arrPop = [1, 2, 3, 4,5,7];
-function popMathod (array){
-    for (i=array.length-1; i>array.length-2; i--){
-      array = array[i]
-    } return array
-  } 
-console.log(popMathod(arrPop));
+let array = [1, 2, 3, 4,5,7,8];
+function pop(array1) {
+  array1.splice(array1.length - 1, 1);
+  return array1
+}
+console.log(pop(array));
 
 //push 1
 let arr = [1, 2, 3, 4,5,7,8];
-function pushMathod (array, value){
+function pushMathod (array){
   arr[arr.length] = 9;
   return arr
 }
