da3c68a04e08b5fc252a3953935c0440e17787ca
Shohruz <shokhruzisroilov@gmail.com>
2022-12-19 15:40:49 +0500

    js homework

--- 9.Arrays/js/script.js
+++ 9.Arrays/js/script.js
@@ -9,16 +9,9 @@ function popMathod (array){
 console.log(popMathod(arrPop));
 
 //push 1
-let arrPush = [1, 2, 3, 4,5,7,8,9];
+let arr = [1, 2, 3, 4,5,7,8];
 function pushMathod (array, value){
-  arrPush = [arrPush,10]
-  return arrPush
+  arr[arr.length] = 9;
+  return arr
 }
-
-console.log(pushMathod(arrPush));
-
-
-//push 2
-let arr = [1,2,3];
-arr[arr.length] = 10;
-console.log(arr);
+console.log(pushMathod(arr));
